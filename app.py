4129932de8cafbbd8028967b61101d33c45c7046
Mike Walmsley <walmsleymk1@gmail.com>
2020-01-18 17:42:22 +0000

    Works on MNIST!

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
-from flask import Flask,render_template,jsonify,request
-from werkzeug import secure_filename
-import base64, os
+import os
+import base64
+from flask import Flask, render_template, jsonify, request
+from werkzeug.utils import secure_filename
+from PIL import Image
+import numpy as np
+import json
 
+import model
 app = Flask(__name__)
 UPLOAD_FOLDER='static'
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/results",methods=['GET','POST'])
 def results():
     if request.method == 'POST':
-        upload_file = request.files['file']
-        filename = secure_filename(upload_file.filename)
-        upload_file.save(os.path.join(UPLOAD_FOLDER, filename))
-        image = cv2.imread(os.path.join(UPLOAD_FOLDER, filename))
-
-        return jsonify(model.predict(app.graph,image))
-
+        print(request)
+        # image_string = request.files['file']
+        data = request.json
+        print(data)
+        image_string = json.loads(data)['file']
+        print(image_string)
+        # filename = secure_filename(upload_file.filename)
+        # upload_file.save(os.path.join(UPLOAD_FOLDER, filename))
+        # image = np.array(Image.open(os.path.join(UPLOAD_FOLDER, filename)))
+        image_bytes = bytes(image_string, encoding='utf-8')
+        image = np.array(Image.frombytes('F', (28, 28), image_bytes, 'raw'))
+        # image_decoded = base64.decodebytes(image_string)
+        # image = np.frombuffer(image_decoded, dtype=np.float64)
+        output = model.predict(image)
+        print(output)
+        return jsonify(output)
     return "Error"
 
-app.graph = model.load_graph('static/model.pb') 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
---
+++ post.py
@@ -0,0 +1,27 @@
+import json
+import base64
+import numpy as np
+import requests
+
+from offline import get_data
+# for debugging
+
+if __name__ == '__main__':
+
+    (train_images, _), _ = get_data()
+    image = train_images[0].astype(np.float32)
+
+    print('Begin post')
+    image_s = base64.b64encode(image)
+    # print(s)
+
+    # post_body = json.dumps()
+    # print(post_body)
+
+    data = json.dumps({'file': image_s.decode('utf-8')})
+    print(data)
+
+
+    r = requests.post('http://0.0.0.0:5000/results', json=data)
+    if r.ok:
+        print(r.json())
