1efa667be01426b3650825e3d8659b7921423d12
Mike Walmsley <walmsleymk1@gmail.com>
2020-01-19 02:23:46 +0000

    Save new images

--- model.py
+++ model.py
@@ -11,8 +11,8 @@ def load_model():
 
 def predict(image):
 
-    image.save('static/latest_input.jpg')
     resized_image = image.resize((224, 224))
+    resized_image.save('static/latest_input.jpg')
     image_arr = np.array(resized_image).astype(np.float32) # drop alpha channel if it exists
     print(image_arr.shape)
     print(image_arr.min(), image_arr.mean(), image_arr.max())  # why all 0's?
@@ -41,7 +41,6 @@ def predict(image):
 
     out = {
             'input_image': 'static/latest_input.jpg',
-            'original_image': 'static/example_content.jpg',
             'styled_image': styled_loc
         }
 
