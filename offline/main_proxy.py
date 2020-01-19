c03284a7d5689bcc18bd0850d81f3790581a5b45
Mike Walmsley <walmsleymk1@gmail.com>
2020-01-19 14:03:08 +0000

    before

--- offline/main_proxy.py
+++ offline/main_proxy.py
@@ -22,7 +22,7 @@ def main(input_image=None):  # an np.array
 
     # https://commons.wikimedia.org/wiki/File:Vassily_Kandinsky,_1913_-_Composition_7.jpg
     # style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')  
-    style_path = 'static/house_fire.jpg'
+    style_path = 'style-imgs/fire_australia.jpg'
   
     # print(input_image.min(), input_image.mean(), input_image.max())
     if input_image is not None:
@@ -101,7 +101,7 @@ def main(input_image=None):  # an np.array
     start = time.time()
 
     epochs = 10
-    steps_per_epoch = 10
+    steps_per_epoch = 15
 
     step = 0
     for n in range(epochs):
